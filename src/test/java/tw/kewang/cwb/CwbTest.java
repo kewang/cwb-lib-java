a59265afd86354ed72e14d9a5e0714221ce8bb44
kewang <cpckewang@gmail.com>
2016-12-30 12:18:36 +0800

    Use env for apiKey

--- src/test/java/tw/kewang/cwb/CwbTest.java
+++ src/test/java/tw/kewang/cwb/CwbTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 public class CwbTest {
     @Before
     public void setup() {
-        Cwb.init("CWB_APIKEY");
+        Cwb.init(System.getenv("CWB_APIKEY"));
     }
 
     @Test
